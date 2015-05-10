b095e3521b045fb1ec810c890716719d309a66a0
Elijah Insua <tmpvar@gmail.com>
2015-05-10 16:15:00 -0400

    better crossing locator
    also includes comments OMG

--- offset-tri.js
+++ offset-tri.js
@@ -42,6 +42,8 @@ var polyline = [
   ]
 ];
 
+//var polyline = [[-10,-100],[-100,-100]]//,[-112,162],[-148,-23],[0,0],[91,28]];
+
 window.dump = function() {
   console.log(JSON.stringify(polyline, null, '  '))
 }
@@ -61,6 +63,25 @@ function line(ctx, x1, y1, x2, y2, color) {
 }
 
 function findCrossing(c, n) {
+
+  /*
+      c     o
+      |     :
+      |     :
+      |     :  a
+      |  b  :
+      |     c
+
+      |_____|
+         r
+
+      c - r == 0
+      b - r == -
+      a - r == +
+  */
+
+
+
   var d0 = c[2] - r;
   var d1 = n[2] - r;
   if (Math.abs(d0) < EPS) {
@@ -71,39 +92,112 @@ function findCrossing(c, n) {
     return n;
   }
 
-  var ret = [
-    c[0] - n[0],
-    c[1] - n[1]
-  ];
+  // ret will eventually be [x, y, sdf(x,y)]
+  var ret = [0, 0];
 
-  var ratio = Math.min(d0, d1) / (d0 - d1)
+  /*
 
-  if (!isFinite(ratio)) {
-    ratio = .5
+    o  d0 (6)
+     \
+      \
+       \
+        o ~~~ isosurface shell (0)
+         \
+          o d1 (-2)
+
+
+    THE ANSWER
+      length of distance interval: 8
+      length of the edge interval: 24
+      the zero crossing (0): .25 (from d1)
+      the guessed crossing on edge:
+        24 * .25 == 6
+        24 - 6 == 18
+
+    in 1d!
+
+  d0 (6)              d1 (-2)
+    o------------o----o
+    +            0    -
+
+                 |____|
+                   .25
+
+
+
+    length = Math.max(6, -2) - Math.min(6, -2) == 8
+    min(6, -2) / (6 - -2)
+
+    ratio (choose one):
+    e1) -2 / 8 == -0.25  (min)
+    e0)  6 / 8 ==  0.75  (max)
+     |
+      \
+       Numerator defines which side to apply the ratio*edge length
+
+
+  e0       e1
+    o-----o
+    0     24
+
+  convert ratio to edge distance
+  e1) 24 * -0.25 == -6
+  e0) 24 *  0.75 ==  18
+
+  apply distance
+  e1) 24 + -6  == 18
+  e0)  0 +  18 == 18
+
+  */
+  var length = Math.max(d0, d1) - Math.min(d0, d1);
+  if (!length) {
+    debugger;
+    return n;
   }
 
-  if (n[0] === c[0]) {
-    if (d0 < d1) {
-      ret[1] = c[1] - ret[1] * ratio;
-    } else if (d0 > d1) {
-      ret[1] = n[1] - ret[1] * ratio;
-    } else {
-      console.log('d0 === d1');
-    }
 
-    ret[0] = c[0];
-  } else if (n[1] === c[1]) {
-    if (d0 < d1) {
-      ret[0] = c[0] - ret[0] * ratio;
-    } else if (d0 > d1) {
-      ret[0] = n[0] - ret[0] * ratio;
+  /*
+    find which orientation this edge is in
+
+    c[0]  n[0]  horizontal edge
+    o-----o
+
+    or vertical edge
+
+    o c[1]
+    |
+    |
+    o n[1]
+  */
+
+  // always choose `c` for the inteval stuffs
+  var ratio = d0 / length;
+
+  // vertical: both `x`s are the same
+  if (n[0] === c[0]) {
+    var edgeLength = Math.max(c[1], n[1]) - Math.min(c[1], n[1]);
+    var amt = edgeLength * ratio;
+    if (sign(c[1]) === sign(amt)) {
+      ret[1] = c[1] - amt;
     } else {
-      console.log('d0 === d1');
+      ret[1] = c[1] + amt;
+    }
+    ret[0] = c[0];
+
+  // horizontal: both `y`s are the same
+  } else if (n[1] === c[1]) {
+    var edgeLength = Math.max(c[0], n[0]) - Math.min(c[0], n[0]);
+    var amt = edgeLength * ratio;
+    if (sign(c[0]) === sign(amt)) {
+      ret[0] = c[0] - amt;
+    } else {
+      ret[0] = c[0] + amt;
     }
     ret[1] = c[1];
   } else {
-    ret[0] = c[0] - ret[0] * ratio;
-    ret[1] = c[1] - ret[1] * ratio;
+    debugger
+    // ret[0] = c[0] - ret[0] * ratio;
+    // ret[1] = c[1] - ret[1] * ratio;
   }
 
   if (isNaN(ret[0]) || isNaN(ret[1])) {
@@ -217,6 +311,8 @@ function gridfill(ctx, r, minx, miny, maxx, maxy, results) {
 
 
       var tests = [[x, y], [x+r, y], [x+r, y+r], [x, y+r]];
+
+      // helps define an edge below
       var potentialCrossings = [
         [0, 1],
         [1, 2],
@@ -243,8 +339,25 @@ function gridfill(ctx, r, minx, miny, maxx, maxy, results) {
         }
       })
 
+      /*
+
+        // Guess
+        -   0    +
+        L---*----U
+            0    +
+            L----U
+
+        // bisect
+        - 0      +
+        L-*------U
+
+        - 0 ++   +
+        L-*-UL---U
+      */
+
       crossings.map(function(c, i) {
         if (c) {
+          // an edge is (x, y) and the distance to the isosurface @ x,y
           var edge = [[
             tests[potentialCrossings[i][0]][0],
             tests[potentialCrossings[i][0]][1],
@@ -258,18 +371,17 @@ function gridfill(ctx, r, minx, miny, maxx, maxy, results) {
 
 
           // bisect the quad edge to find the closest point to zero-crossing
-          var ssss = 100, d = c, updateIndex;
+          var ssss = 10, d = c, updateIndex;
           var lastDistance = Infinity;
-          var mid;
+          var mid = [0, 0];
           var midpointDistance;
           while(ssss--) {
             // bisect the quad current edge
             mid = findCrossing(edge[0], edge[1]);
             midpointDistance = sdf(mid[0], mid[1]);
-            console.log(midpointDistance)
             mid.push(midpointDistance);
 
-            if (Math.abs(midpointDistance - r) < .00001 || midpointDistance >= lastDistance) {
+            if (Math.abs(midpointDistance - r) < 1e-6) {
               found = true;
               ctx.beginPath()
                 circle(ctx, mid[0], mid[1], 1);
@@ -280,21 +392,73 @@ function gridfill(ctx, r, minx, miny, maxx, maxy, results) {
               break;
             }
 
-            updateIndex = closest(edge[0][2], edge[1][2], r);
-            edge[updateIndex][0] = mid[0];
-            edge[updateIndex][1] = mid[1];
-            edge[updateIndex][2] = midpointDistance;
+            /*
+
+              we a guess with it's distance to the isosurface
+
+              +          -
+              o------*---o
+
+              then we compute the distance for the new point
+
+              +      -   -
+              o------*---o
+
+              find the two points that still contain the crossing
+
+              +      -   -
+              o------*---o
+
+              |______|
+
+                these become the new edge
+
+
+
+
+              +      -   -   -   -
+              o------o---*---*---*
+                  |
+                  bisect
+            */
+
+            if (sign(midpointDistance - r) !== sign(edge[0][2] - r)) {
+              edge[1][0] = mid[0];
+              edge[1][1] = mid[1];
+              edge[1][2] = mid[2];
+            } else if (sign(midpointDistance - r) !== sign(edge[1][2] - r)) {
+              edge[0][0] = mid[0];
+              edge[0][1] = mid[1];
+              edge[0][2] = mid[2];
+            } else {
+              // potentially all positive or all negative
+              debugger;
+            }
+
+            // if (sign(midpointDistance) === edge[0][1]) {
+            //   edge[1] = mid;
+            // } else {
+            //   edge[0] = mid;
+            // }
+
+            // updateIndex = closest(edge[0][2], edge[1][2], r);
+            // edge[updateIndex][0] = mid[0];
+            // edge[updateIndex][1] = mid[1];
+            // edge[updateIndex][2] = midpointDistance;
           }
 
           if (ssss <= 0) {
-            console.log('ran out of runway', Math.abs(midpointDistance - r));
+            console.log('ran out of runway', mid[0], mid[1], midpointDistance - r, edge[0][2] - r, edge[1][2] - r);
             // contour.push([mid, x, y, midpointDistance]);
             // contour.push([[edge[0][0], edge[0][1]], x, y, edge[2]]);
             // contour.push([[edge[1][0], edge[1][1]], x, y, edge[2]]);
-            ctx.beginPath()
-              circle(ctx, mid[0], mid[1], 10);
-              ctx.fillStyle = "orange";
-              ctx.fill();
+            // ctx.beginPath()
+            //   circle(ctx, mid[0], mid[1], 10);
+
+
+              ctx.fillStyle = "rgba(255, 123, 54, .25)";
+              ctx.fillRect(x+5, y+5, r-10, r-10);
+              // ctx.fill();
           }
         }
       })
@@ -304,6 +468,7 @@ function gridfill(ctx, r, minx, miny, maxx, maxy, results) {
   var gridpoints = {};
 
   var points = {};
+  // poor man's cache for running point -> cell queries
   contour.forEach(function(point) {
     var gridkey = point[1] + ',' + point[2];
     if (!gridpoints[gridkey]) {
@@ -343,8 +508,37 @@ function gridfill(ctx, r, minx, miny, maxx, maxy, results) {
           continue;
         }
 
-        var mid = findCrossing(ip[0], jp[0]);
+        /*
+           a
+        o--*---o
+        | /    * c
+      b *     /|
+        o----*-o
+             d
+
+        a-------c
+
+        find the midpoint
+
+        a---o---c
+
+        test distance at o
+
+        0   +   0
+        a---o---c   = not connected
+
+        0   0   0
+        a---o---c   = connected
+
+        0   -   0
+        a---o---c   = not connected
+
+        */
+
+
+        var mid = bisect(ip[0], jp[0]);
         var midd = sdf(mid[0], mid[1]) - r
+        return;
         if (midd <= r/4) {
           line(ctx, jp[0][0], jp[0][1], ip[0][0], ip[0][1]);
           ctx.strokeStyle = "red";
@@ -390,11 +584,14 @@ function gridfill(ctx, r, minx, miny, maxx, maxy, results) {
 var r = 30;
 var b = [0, 0, 0, 0];
 var ctx = fc(function() {
+
+  // canvas scene setup
   ctx.clear();
   center(ctx);
 
   bounds2(polyline, b);
 
+  // compute and draw grid lines
   b[0] = ((Math.floor(b[0]/r) * r) - r*2)|0;
   b[1] = ((Math.floor(b[1]/r) * r) - r*2)|0;
   b[2] = ((Math.ceil(b[2]/r) * r) + r*2)|0;
@@ -412,12 +609,14 @@ var ctx = fc(function() {
   var results = [];
   gridfill(ctx, gridspacing, b[0], b[1], b[2], b[3], results);
 
+  // draw the polygon
   ctx.beginPath();
     poly(ctx, polyline);
   ctx.closePath();
   ctx.strokeStyle = "hsl(17, 80%, 56%)";
   ctx.stroke();
 
+  // draw the polygon points
   ctx.beginPath();
     points(ctx, 3, polyline)
     ctx.fillStyle = "hsl(49, 60%, 56%)";
